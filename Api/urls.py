38e9a01ab501d23715230dc148f9a7247b3cbbbf
nonymous01 <youssef.anonymous0@gmail.com>
2024-02-28 14:11:56 +0000

    e

--- Api/urls.py
+++ Api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', Apiviews, name='Apiviews'),
+    path('views/', Apiviews, name='Apiviews'),
     path('create/',views_item, name='views_item'),
     path('all/',afficher_all, name="afficher_all"),
     path('update/<int:pk>/', updates, name="updates"),
